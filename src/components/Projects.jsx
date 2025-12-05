b56bcc28a9cc5299c838f21bd553ed7b9125f92a
Roxanna <20186170@itla.edu.do>
2025-12-05 17:19:59 -0400

    adding a new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -29,9 +29,9 @@ export const Projects = () => {
                         <span>React</span>
                         <span>CSS</span>
                         <span>HTML</span>
+                        <span>JavaScript</span>
                         <span>Vite</span>
                         <span>Vercel(CI/CD)</span>
-                        <span>JavaScript</span>
                     </div>
                     <a href="https://github.com/Roxannaa/social-media-links" target='_blank' class="arrow-link">
                         View Project  <span><i class="fa-solid fa-arrow-right"></i></span>
@@ -54,21 +54,23 @@ export const Projects = () => {
                         View Project  <span><i class="fa-solid fa-arrow-right"></i></span>
                     </a>
                  </motion.div>
-                 {/* <motion.div className='project-card' variants={fadeInUp} whileHover={{y:-10, transition: {duration: 0.2}}}>
-                    <h3> ⛓️‍💥 Personal Social Links Card </h3>
+                 <motion.div className='project-card' variants={fadeInUp} whileHover={{y:-10, transition: {duration: 0.2}}}>
+                    <h3> 🌦️ Weather App </h3>
                     <p>
-                        A simple and responsive React app designed to share who I am and where to find me online. It features a clean, modern interface that highlights my personal information, photo, and social media links in one accessible place.
+                        A simple and intuitive weather application that allows users to check real-time weather conditions by searching for any city. The app retrieves temperature, weather description, and other key details using the OpenWeather API. 
                     </p>
                     <div className='project-tech'>
-                        <span>React</span>
+                         <span>React</span>
                         <span>CSS</span>
                         <span>HTML</span>
                         <span>JavaScript</span>
+                        <span>Vite</span>
+                        <span>Vercel(CI/CD)</span>
                     </div>
-                    <a href="#" class="arrow-link">
+                    <a href="https://github.com/Roxannaa/weather-app" target='_blank' class="arrow-link">
                         View Project  <span><i class="fa-solid fa-arrow-right"></i></span>
                     </a>
-                 </motion.div> */}
+                 </motion.div>
             </motion.div>
         </motion.section>
     );
